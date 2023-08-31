58449fd300ec2b8c399a0b61b4cd52a166ace9ac
KrisLunara <9ut4@fresnou.org>
2023-08-31 09:18:00 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index',{
+    color: "#FFFFFF"
+  });
+});
+
+module.exports = router;
+router.post('/', function(req, ){
+  res.render('result', {
+    color: req.body.colorPicker
+  })
+});
